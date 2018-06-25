4422922e75b6a799ab0968fb42b5c49c68f192cf
Kevin Crawford <kcdouglas92@gmail.com>
2018-06-25 14:04:40 -0700

    commit nhl hockey quiz

---
+++ index.js
@@ -0,0 +1,292 @@
+const STORE = [
+  { 
+    question: 'Which official has the final decision in matters of disputed goals?',
+    answers: [
+    'Video Goal Judge',
+    'Linesman', 
+    'Referee', 
+    'Goal Judge'],
+    correctAnswer: 'Referee'
+  },
+  {
+    question: 'A player is allowed to direct the puck to a teammate with an open hand, also known as a hand pass, if:',
+    answers: [
+      'Both players are in their defensive zone',
+      'Both players are in the neutral zone',
+      'The pass originates in the defensive zone',
+      'This is never allowed'
+      ],
+      correctAnswer: 'Both players are in their defensive zone',
+  },
+  {
+    question: 'If a delayed penalty is signaled by the referee and the non-offending team subsequently ices the puck, where does the face off take place?',
+    answers: [
+      'At the end face-off spot of the team icing the puck',
+      'At center ice',
+      'At the end zone face-off spot of the team who committed the penalty',
+      'In the neutral zone near the defending blue line of the team icing the puck'
+      ],
+      correctAnswer: 'In the neutral zone near the defending blue line of the team icing the puck',
+  },
+  {
+    question: 'In which of the following situations would a goal NOT be allowed?',
+    answers: [
+      'When the puck is deflected directly into the net off a teammate',
+     'When the puck is deflected directly into the net off an official',
+    'When the puck is deflected directly into the net off a player of the opposing team',
+    'When the puck is deflected directly into the net off a dropped stick'
+    ],
+    correctAnswer: 'When the puck is deflected directly into the net off an official',
+  },
+  {
+    question: 'A goalkeeper is not allowed to participate in the play in any manner where?',
+    answers: [
+      'Inside the face-off circles',
+      'Beyond the blue line indicating his defensive zone',
+      'Beyond the center red line',
+      'Behind his own net'
+      ],
+    correctAnswer: 'Beyond the center red line',
+  },
+  {
+    question: 'The 12-inch wide blue lines are considered part of what zone?',
+    answers: [
+      'Whatever zone the puck is in',
+      'No man\'s land',
+      'The end zones',
+      'None - the line itself is separate from any zone'
+      ],
+    correctAnswer: 'Whatever zone the puck is in',
+  },
+  {
+    question: 'According to the USA Hockey Rule Book, what is the act of a player swinging his stick at an opponent, whether contact is made, or not?',
+    answers: [
+      'Hooking',
+      'Slashing',
+      'Spearing',
+      'Diving'
+      ],
+    correctAnswer: 'Slashing',
+  },
+  {
+    question: 'If a player prevents an opponent who has dropped his stick or any other piece of equipment from regaining possession of it, what is the proper penalty?',
+    answers: [
+      'Unsportsmanlike conduct',
+      'Interference',
+      'Misconduct',
+      'Holding'
+      ],
+    correctAnswer: 'Interference',
+  },
+  {
+    question: 'A team has two players serving penalties. A third player is subsequently called for a penalty. What happens?',
+    answers: [
+      'The player goes to the penalty box, a substitute takes his place on the ice, and his penalty time begins immediately',
+      'The player goes to the penalty box, a substitute takes his place on the ice, and his penalty time does not begin until one of the other penalties expires',
+      'The player goes to the penalty box, the team\'s numerical strength is further reduced by one, and his penalty time begins immediately',
+      'The other team is awarded a penalty shot'
+      ],
+    correctAnswer: 'The player goes to the penalty box, a substitute takes his place on the ice, and his penalty time does not begin until one of the other penalties expires',
+  },
+  {
+    question: 'The signal given for a misconduct penalty is:',
+    answers: [
+      'Palm of hand on head',
+      'Making a "T" sign with the hands',
+      'Both hands on hips',
+      'One arm raised'
+      ],
+    correctAnswer: 'Both hands on hips',
+  },
+  ];
+  
+  let questionNum = 0;
+  let goalsScored = 0;
+
+//GLOBAL functions
+
+// updates current question counter
+  function updateQuestionCount() {
+    questionNum++;
+      $('.question-current').text(questionNum+1);
+  }
+  
+// updates questions answered correctly count
+  function updateGoalsScored() {
+    goalsScored++;
+    
+  }
+  
+// Posts updated goals scored to DOM
+  function postGoalsScored() {
+    updateGoalsScored();
+    $('.scored').text(goalsScored);
+  }
+  
+  
+  // START MENU ITEMS
+  
+  // Handles on click functions of START Button. 
+  // hides Div of Start Quiz Menu
+  // unhides div for quiz form
+  
+  function startQuiz() {
+     $('.start-menu').on('click', '.start-button', function (event) {
+    $('.start-menu').remove();
+    postQuestion();
+    $('.question-current').text(questionNum+1);
+    });
+    console.log('`startQuiz` ran');
+  }
+  
+  
+  
+ // QUESTION MENU ITEMS
+  
+  // compiles HTML question form in JQUERY
+  function createQuestion() {
+    return `<div class="question-menu">
+    <h2>${STORE[questionNum].question}</h2>
+   <form id="js-question-form" class="question-form">
+    <fieldset name="question-field">
+      <label class="answer-option">
+      <input type="radio" value="${STORE[questionNum].answers[0]}" name="answer" required />
+        <span>${STORE[questionNum].answers[0]}</span><br>
+     </label>
+      <label class="answer-option">
+      <input type="radio" value="${STORE[questionNum].answers[1]}" name="answer" required />
+        <span>${STORE[questionNum].answers[1]}</span><br>
+      </label>
+       <label class="answer-option">
+      <input type="radio" value="${STORE[questionNum].answers[2]}" name="answer" required />
+        <span>${STORE[questionNum].answers[2]}</span><br>
+      </label>
+      <label class="answer-option">
+      <input type="radio" value="${STORE[questionNum].answers[3]}" name="answer" required />
+        <span>${STORE[questionNum].answers[3]}</span><br>
+      </label>
+    <button type="submit" class="submit-button">SUBMIT</button>
+      </fieldset>
+    </form>
+  </div>`;
+  }
+  
+  // post question to DOM
+  function postQuestion() {
+    $('.question-feedback-target').html(createQuestion());
+    console.log('`postQuestion` ran');
+  }
+  
+  //user selects an answer, submits, returns feedback
+  function selectAnswer() {
+    $('.question-feedback-target').on('submit', '#js-question-form', function(event) {
+      event.preventDefault();
+      console.log(`yay form submitted`);
+      
+      let trueAnswer = `${STORE[questionNum].correctAnswer}`;
+      let selectedAnswer = $('input[name=answer]:checked', '#js-question-form').val();
+      
+    if (selectedAnswer === trueAnswer) {
+      ifCorrectAnswer(); 
+    }
+    else {
+      ifIncorrectAnswer(); 
+    }
+    });
+    console.log('`selectAnswer` ran');
+  }
+  
+  
+  
+// FEEDBACK MENU ITEMS
+  
+  // if correct, invoke correctAnswerFeedback, updateGoalsScored
+  function ifCorrectAnswer() {
+    postGoalsScored();
+    $('.question-menu').remove();
+     $('.question-feedback-target').html(correctAnswerFeedback());
+  }
+  
+  // if incorrect, invoke incorrectAnswerFeedback
+  function ifIncorrectAnswer() {
+    $('.question-menu').remove();
+    $('.question-feedback-target').html(incorrectAnswerFeedback());
+  }
+  
+  // generates correctAnswerFeedback
+  function correctAnswerFeedback () {
+    return `<div class="feedback-menu">
+    <img src="http://jingletree.com/minnmixnew/images/siren.gif" alt="hockey-goal-siren">
+        <h2>
+          Correct!
+        </h2>
+        <p>The correct answer was "${STORE[questionNum].correctAnswer}"</p>
+        <button type="button" class="next-button">NEXT</button>
+        </div>`;
+  }
+  
+  // generates incorrectAnswerFeedback
+  function incorrectAnswerFeedback () {
+    return `<div class="feedback-menu">
+        <h2>
+          Oops! Better luck next time.
+        </h2>
+        <p>The correct answer was "${STORE[questionNum].correctAnswer}" </p>
+        <button type="button" class="next-button">NEXT</button>
+    </div>`;
+  }
+  
+  // will post next question on click of NEXT button 
+  function postNextQuestion () {
+    $('.question-feedback-target').on('click', '.next-button', function (event) {
+      if (questionNum < STORE.length -1) {
+      updateQuestionCount();
+      createQuestion();
+      postQuestion();
+    } else {
+      postResults();
+    }
+  });
+    console.log('`postNextQuestion` ran');
+  }
+  
+  
+// RESULTS MENU ITEMS
+  
+  // posts results of quiz, score
+  function postResults () {
+    $('.feedback-menu').remove();
+    $('.question-feedback-target').html(`<div class="results-menu">
+      <img id="finished-quiz-img" src="https://static.tumblr.com/0ff1d35c32bd472d78be3a4cb5279812/fjhd4ng/pAIn5fyqw/tumblr_static_filename_640_v2.jpg" alt="hockey celly">
+        <h2>
+          Congratulations!
+        </h2>
+        <p>You scored ${goalsScored} goals on 10 attempts.</p>
+        <button type="button" class="restart-button">Restart Quiz?</button>
+    </div>`);
+  }
+  
+ // restart quiz through reload page function
+  function restartQuiz () {
+    $('.question-feedback-target').on('click', '.restart-button', function (event) {
+      location.reload();
+    });
+  }
+
+  
+  
+// EXECUTE QUIZ functions
+function handleQuiz() {
+  startQuiz();
+  selectAnswer();
+  postNextQuestion();
+  restartQuiz();
+}
+  
+$(handleQuiz);
+  
+  
+  
+  
+  
+  
